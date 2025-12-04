6fbf044e62d3f06c0b66b3e1032f617c79be5e80
Tony Parker <161804998+TonyParker5@users.noreply.github.com>
2025-12-04 09:05:17 +0200

    Add files via upload

---
+++ src/pages/components/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+
+    
+  return (
+    <div>
+        <div  className="header">
+            <div  className="container">
+       
+                <h1 style={{textAlign:"center", background:'white', border:"none"}}>تركة - لحساب قيمة الميراث</h1>
+                <p>*نموذج أولي*</p>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
